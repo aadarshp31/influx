3c8999451ee90ef6b7329ce8af49c266cbc0702e
aadarshp31 <44257152+aadarshp31@users.noreply.github.com>
2023-12-31 23:06:38 +0530

    chore: added env variables and constants for server

---
+++ server/src/main/java/com/app/server/common/CONSTANT.java
@@ -0,0 +1,15 @@
+package com.app.server.common;
+
+/**
+ * Class to contains all constants used in the application
+ * 
+ * @author @aadarshp31
+ */
+public class CONSTANT {
+  // table name constants
+  public final static String USER_ROLE_JUNCTION_TABLE_NAME = "user_role_junction";
+
+  // Role related contants
+  public final static String ROLE_ADMINISTRATOR = "ADMIN";
+  public final static String ROLE_USER = "USER";
+}
