0537416d974a0bcc34a940986cfb2513b5d01227
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2024-01-06 23:45:38 +0530

    added methods to extract claims and validate JWT tokens in "JwtService" class

--- server/src/main/java/com/app/server/authentication/JwtService.java
+++ server/src/main/java/com/app/server/authentication/JwtService.java
@@ -2,12 +2,14 @@ package com.app.server.authentication;
 
 import java.util.Date;
 import java.util.HashMap;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.Jwts.SIG;
 import io.jsonwebtoken.io.Decoders;
@@ -61,6 +63,72 @@ public class JwtService {
         .claims(extraClaims)
         .signWith(getSecretKey(), SIG.HS256)
         .compact();
+  }
+
+  /**
+   * Gets all claims fron a JWT token
+   * 
+   * @param jwtToken JWT token
+   * @return All claims present in the JWT token
+   * @author @aadarshp31
+   */
+  public Claims extractAllClaims(String jwtToken) {
+    return Jwts
+        .parser()
+        .verifyWith(getSecretKey())
+        .build()
+        .parseSignedClaims(jwtToken)
+        .getPayload();
+  }
+
+  /**
+   * Extracts s specific claim from JWT token using a resolver function
+   * 
+   * @param <T>           data type of the claim you want to extract
+   * @param jwtToken      JWT token
+   * @param claimResolver a function that helps to get a particular claim from a
+   *                      list of claims
+   * @return specific claim that the claimResolver function returns
+   * @author @aadarshp31
+   */
+  public <T> T extractClaim(String jwtToken, Function<Claims, T> claimResolver) {
+    Claims claims = extractAllClaims(jwtToken);
+    return claimResolver.apply(claims);
+  }
+
+  /**
+   * Extracts username from the JWT token
+   * 
+   * @param jwtToken JWT token
+   * @return username stored in the JWT token
+   * @author @aadarshp31
+   */
+  public String extractUsername(String jwtToken) {
+    return extractClaim(jwtToken, Claims::getSubject);
+  }
+
+  /**
+   * Extracts expiry from the JWT token
+   * 
+   * @param jwtToken JWT token
+   * @return expiry time of the JWT token
+   * @author @aadarshp31
+   */
+  public Date extractExpiry(String jwtToken) {
+    return extractClaim(jwtToken, Claims::getExpiration);
+  }
+
+  /**
+   * Checks whether the provided JWT token is valid or not
+   * 
+   * @param jwtToken    JWT token
+   * @param userDetails userDetails object of the requesting user
+   * @return boolean indicating whether the provided JWT token is valid or not
+   * @author @aadarshp31
+   */
+  public Boolean isValidToken(String jwtToken, UserDetails userDetails) {
+    String subject = extractUsername(jwtToken);
+    return userDetails.getUsername().equals(subject);
   }
 
 }
