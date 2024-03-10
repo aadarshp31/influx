98d0c0382c8f08fa4987f35a247bf35c004ffba5
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2024-03-10 15:17:22 +0530

    fix(env): env variables not exposed properly in client app

--- client/src/lib/CONSTANTS.ts
+++ client/src/lib/CONSTANTS.ts
@@ -1,5 +1,5 @@
 export default class CONSTANTS {
-  static readonly API_URL = process.env.REACT_APP_API_URL || 'http://localhost:4000/api';
+  static readonly API_URL = import.meta.env.REACT_APP_API_URL || 'http://localhost:4000/api';
   static readonly SIGNUP_API = '/auth/signup';
   static readonly LOGIN_API = '/auth/signin';
   static readonly USER_API = '/users';
