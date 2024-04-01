dbcfbaa3e2fb0172e41a82f46a0f4bdbcf007852
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2024-04-01 22:25:52 +0530

    fix: login button UI text not visible on hover

--- client/src/components/pages/HomePage/layouts/AuthModal.tsx
+++ client/src/components/pages/HomePage/layouts/AuthModal.tsx
@@ -36,7 +36,7 @@ function AuthModal({}: Props) {
 					<AlertDialogTrigger asChild>
 						<Button
 							variant='outline'
-							className='mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+							className='mt-4 w-full rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
 						>
 							Login
 						</Button>
