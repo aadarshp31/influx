79e7d85856710b307679856cdff821a735866232
Adarsh Pandey <44257152+aadarshp31@users.noreply.github.com>
2024-04-01 22:10:56 +0530

    feat: hide/show login logout buttons based of the user login status at client side

--- client/src/components/pages/HomePage/layouts/AuthModal.tsx
+++ client/src/components/pages/HomePage/layouts/AuthModal.tsx
@@ -26,7 +26,7 @@ function AuthModal({}: Props) {
 					<AlertDialogTrigger asChild>
 						<Button
 							variant='outline'
-							className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
+							className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'
 						>
 							Login
 						</Button>
